744fab027563ab381c2ac6aab90f349e84936d44
ravihw7 <ravihwoff@gmail.com>
2026-09-21 18:50:57 +0530

    Added Logo and style to Heading

--- src/components/header.py
+++ src/components/header.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
+
 def header_home():
-    st.header("RavClass - AI Attendance System")
+
+    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
+    
+    st.markdown(f"""
+        <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:30px; margin-top:30px">
+            <img src='{logo_url}' style='height:100px;' />
+            <h1 style='text-align:center; color:#E0E3FF'>RavClass</h1>
+            <h2 style='text-align:center; color:#E0E3FF'>AI Attendance System</h2>
+        </div>   
+                
+                """, unsafe_allow_html=True)
+
