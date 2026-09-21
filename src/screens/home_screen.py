c010dc0cee07576b7bd5a9b5c8d351d4aece5176
ravihw7 <ravihwoff@gmail.com>
2026-09-21 18:50:05 +0530

    Imported the File Functions

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -1,11 +1,16 @@
 import streamlit as st
 from src.components.header import header_home
-from src.ui.base_layout import base_layout
+from src.ui.base_layout import style_base_layout
+from src.ui.base_layout import style_background_home
+from src.ui.base_layout import style_background_dashboard
 
 def home_screen():
 
     header_home()
-    base_layout()
+    style_background_home()
+    style_base_layout()
+    style_background_dashboard()
+    
     col1, col2 =st.columns(2) 
     with col1:
         if st.button("Teacher Login"):
