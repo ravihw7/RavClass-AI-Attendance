9772733cf2712ee09048ad82bc6699c469f79de8
ravihw7 <ravihwoff@gmail.com>
2026-09-22 22:04:34 +0530

    made few changes in the code

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-def teacher_screen():
-    st.header("Teacher Screen")
+from src.ui.base_layout import style_background_dashboard, style_base_layout
+
+def main():
+    style_base_layout()
+    style_background_dashboard()
+    st.title("Teacher Screen")
